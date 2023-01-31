edfc12e3d3f9099aedea26f8becbf732cb888d8f
Jaxon Pecora <72949814+Nameless95@users.noreply.github.com>
2023-01-31 14:18:11 -0500

    added mph tracker

---
+++ bobsled/Assets/Scripts/speed.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Diagnostics;
+using System.Threading;
+using System;
+using Valve.VR;
+
+
+public class speed : MonoBehaviour
+{
+    public TMP_Text text;
+    public Rigidbody rb;
+  
+    
+
+
+    
+  
+
+
+
+ 
+
+        // Update is called once per frame
+    void Update()
+    {   
+        
+
+
+        double speed = rb.velocity.magnitude * 2.237;
+        string mph = "MPH";
+        text.text = speed.ToString("00") + mph;
+
+
+      
+    }
+}
+
