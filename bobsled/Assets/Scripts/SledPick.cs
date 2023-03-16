789743009ff7d8e3427b3c7b644945e83f7f2cde
sean castanon <sc7295@gmail.com>
2023-03-16 16:30:04 -0400

    Sled Menu Scene Added
    New model and animation
    button mesh needs to change when button is clicked
    No way to get into the main scene yet though

---
+++ bobsled/Assets/Scripts/SledPick.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SledPick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject sled_turntable;
+    public int no_of_sleds;
+    void Start()
+    {
+        no_of_sleds = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+
+    public void leftArrowClick()
+    {
+        onArrowPress('l');
+    }
+    public void rightArrowClick()
+    {
+        onArrowPress('r');
+    }
+    void onArrowPress(char arrow)//- = left, + right
+    {
+        int scalar;
+        if (arrow == 'l')
+            scalar = -1;
+        else if (arrow == 'r')
+            scalar = 1;
+        float rotation = 360 / no_of_sleds;
+        sled_turntable.transform.Rotate(0f, rotation, 0f, Space.World);
+    }
+}
